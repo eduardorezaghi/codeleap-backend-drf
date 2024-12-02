dcad863c7ed3011c0833f5d79172144d778bc9f7
Eduardo Rezaghi <eduardo.rezaghi@gmail.com>
2024-12-02 20:16:54 -0300

    Simplify model schema.

--- src/api/models.py
+++ src/api/models.py
@@ -1,19 +1,25 @@
 from django.db import models
 
-class User(models.Model):
-    username = models.CharField(max_length=255)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+# TODO: We could use this model if we wanted to have a granular control over the users
+# class User(models.Model):
+#     username = models.CharField(max_length=255)
+#     created_at = models.DateTimeField(auto_now_add=True)
+#     updated_at = models.DateTimeField(auto_now=True)
 
 
-    def __str__(self):
-        return self.username
+#     def __str__(self):
+#         return self.username
 
 class Post(models.Model):
     title = models.CharField(max_length=255)
+    username = models.CharField(max_length=255, default='Anonymous', blank=True)
     content = models.TextField()
     created_datetime = models.DateTimeField(auto_now_add=True)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    updated_datetime = models.DateTimeField(auto_now=True)
+    # soft delete
+    deleted_at = models.DateTimeField(null=True, blank=True)
+    # TODO: We could use this field if we wanted to have a granular control over the users
+    # user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     class Meta:
         ordering = ['-created_datetime']
