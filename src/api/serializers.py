7c4e1e54907de5985425e8644b69866590891f76
Eduardo Rezaghi <eduardo.rezaghi@gmail.com>
2024-12-02 21:55:10 -0300

    Refactor serializers to comply with challenge model.

--- src/api/serializers.py
+++ src/api/serializers.py
@@ -1,8 +1,35 @@
 from rest_framework import serializers
+
 from .models import Post
+
 
 class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
-        fields = ['id', 'user','title', 'content', 'created_datetime']
-        read_only_fields = ['id', 'user', 'created_datetime']
+        fields = [
+            "id",
+            "title",
+            "username",
+            "content",
+            "created_datetime",
+        ]
+        read_only_fields = ["id", "created_datetime", "updated_datetime"]
+
+
+class PostListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Post
+        fields = ["id", "username", "created_datetime", "title", "content"]
+
+
+class PostCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Post
+        fields = ["title", "username", "content"]
+        read_only_fields = ["username"]
+
+
+class PostUpdateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Post
+        fields = ["title", "content", "updated_datetime"]
