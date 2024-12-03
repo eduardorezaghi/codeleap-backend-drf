da25a6c5e6a681c325631e6bc05e4455ebc99975
Eduardo Rezaghi <eduardo.rezaghi@gmail.com>
2024-12-02 21:56:23 -0300

    Organize views.py to use ModelViewSet with get_serializer_class Serializer mapper.
     - Do not allow PUT method. Only allow partial updates.

--- src/api/views.py
+++ src/api/views.py
@@ -1,18 +1,24 @@
-from django.db.models import QuerySet
-from rest_framework.views import APIView
-from rest_framework.response import Response
+from rest_framework import status, viewsets
 from rest_framework.permissions import AllowAny
-from rest_framework import viewsets
-from rest_framework.decorators import action
-from rest_framework.request import Request
 from rest_framework.response import Response
-from rest_framework.permissions import IsAuthenticated
-from rest_framework.exceptions import NotFound
+
+from api.serializers import PostCreateSerializer, PostSerializer, PostUpdateSerializer
+
 from .models import Post
-from .serializers import PostSerializer
 
 
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
     permission_classes = [AllowAny]
+
+    def get_serializer_class(self):
+        if self.action == "create":
+            return PostCreateSerializer
+        elif self.action in ["update", "partial_update"]:
+            return PostUpdateSerializer
+        return PostSerializer
+
+    def update(self, request, *args, **kwargs):
+        response = {"detail": 'Method "PUT" not allowed.'}
+        return Response(response, status=status.HTTP_405_METHOD_NOT_ALLOWED)
