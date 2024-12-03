0f7363b2332707eb8206fdafee67ea95ce7e1487
Eduardo Rezaghi <eduardo.rezaghi@gmail.com>
2024-12-02 21:55:29 -0300

    Use /careers/ route mapping.

--- src/api/urls.py
+++ src/api/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 from .views import PostViewSet
 
+router = DefaultRouter()
+router.register(r'', PostViewSet, basename='post')
+
 urlpatterns = [
-    path('', PostViewSet.as_view({
-        'get': 'list',
-        'post': 'create'
-    })),
+    path('', include(router.urls)),
 ]
